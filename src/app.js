6ac88dc6419412cfa189423f8db901de388f5d74
Hunain Mehmood <hunain60@gmail.com>
2021-08-13 03:16:33 -0400

    added integration tests

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const logger = require('morgan');
-const primenessRouter = require('../routes/primeness.route');
-const apiDocs = require('../routes/apidocs.route')
+const primenessRouter = require('./routes/primeness.route');
+const apiDocs = require('./routes/apidocs.route')
 const port = process.env.NODE_HOST_PORT || 5000;
 const cors = require("cors");
 
--- test/primeness.test.js
+++ test/primeness.test.js
@@ -2,12 +2,45 @@ const assert = require('assert');
 var rewire = require('rewire');
 var primenessController = rewire('../src/controllers/primeness.controller');
 var checkIfNumberIsPrime = primenessController.__get__('checkIfNumberIsPrime')
+var app = require("../src/app")
+var chai = require('chai')
+  , chaiHttp = require('chai-http');
+
+chai.use(chaiHttp);
+
+
 
 describe('Check Prime Number', () => {
  it('should return true', () => {
         assert.equal(true, checkIfNumberIsPrime(2));
     });
  it('should return false', () => {
-        assert.equal(true, checkIfNumberIsPrime(1));
+        assert.equal(false, checkIfNumberIsPrime(1));
+    });
+});
+
+describe('/GET checkPrime/:number', () => {
+    it('it should return true', (done) => {
+      chai.request(app)
+          .get('/primeness/checkPrime/2')
+          .end((err, res) => {
+            chai.expect(err).to.be.null;
+            chai.expect(res).to.have.status(200);
+            chai.expect(res.text).to.equal("true")
+            done();
+          });
+    });
+});
+
+describe('/GET checkPrime/:number', () => {
+    it('it should return false', (done) => {
+      chai.request(app)
+          .get('/primeness/checkPrime/1')
+          .end((err, res) => {
+            chai.expect(err).to.be.null;
+            chai.expect(res).to.have.status(200);
+            chai.expect(res.text).to.equal("false")
+            done();
+          });
     });
 });
